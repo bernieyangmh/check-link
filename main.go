5cd2dea49d0906edb080004c316bf84d0f231350
bernieyangmh <berniey@163.com>
2018-01-08 21:30:05 +0800

    extract src_href array to waitUrlMap

--- main.go
+++ main.go
@@ -1,142 +1,222 @@
 package main
 
 import (
-	"net/http"
-	"io/ioutil"
-	"fmt"
-	"regexp"
 	"bytes"
-	"github.com/golang-collections/go-datastructures/set"
-	//"log"
+	"fmt"
+
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
 )
 
-
-
 const (
-	PATTERN_SRC = `src=\"(.*?)\"`
-	PATTERN_HERF = `href=\"(.*?)\"`
-	PATTERN_HTTP = `http(.*?)`
-	PATTERN_LINK = `https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`
+	PATTERN_SRC   = `src=\"(.*?)\"`
+	PATTERN_HERF  = `href=\"(.*?)\"`
+	PATTERN_HTTP  = `http(.*?)`
+	PATTERN_LINK  = `https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`
 	PATTERN_SLASH = `\/(.*?)`
 )
 
-
 func main() {
-	test_num := 0
-	resp, err := http.Get("http://www.qiniu.com")
-	if err != nil {
-		// handle error
+
+	waitUrlMap := make(map[string]int)
+	finishUrlMap := make(map[string]int)
+
+
+	respBody, StatusCode, ContentType := Crawling("http://www.qiniu.com")
+
+	finishUrlMap["http://www.baidu.com"] = StatusCode
+
+	if ContentType != "text/html; charset=utf-8" {
+		
 	}
+		
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		// handle error
-	}
-
-
-	reHref, _ := regexp.Compile(PATTERN_HERF)
-	reSrc, _ := regexp.Compile(PATTERN_SRC)
-	reHttp, _ := regexp.Compile(PATTERN_HTTP)
-	reLink, _ := regexp.Compile(PATTERN_LINK)
-	reSlash, _ := regexp.Compile(PATTERN_SLASH)
-
-	hrefArray := reHref.FindAllStringSubmatch(string(body), 10000)
-	srcArray := reSrc.FindAllStringSubmatch(string(body), 10000)
-
-	waitUrlSet := set.New()
-	//finishSet := set.New()
+	hrefArray, srcArray := ExtractBody(respBody)
 
 
 	for i := 0; i < len(srcArray); i++ {
-		var srcLogBuffer bytes.Buffer
 		srcOriginLink := srcArray[i][1]
 
-		srcLogBuffer.WriteString("srcOriginLink is ")
-		srcLogBuffer.WriteString(srcOriginLink)
-
-		if reSlash.MatchString(srcOriginLink) != true {
+		if ReHaveSlash(srcOriginLink) != true {
 			continue
 		}
 
-		if reHttp.MatchString(srcOriginLink) != true {
+		if ReIsHttp(srcOriginLink) != true {
 
-			var resUrlBuffer bytes.Buffer
-			resUrlBuffer.WriteString("http://www.qiniu.com")
-			resUrlBuffer.WriteString(srcOriginLink)
-			resLink := resUrlBuffer.String()
+			resLink := StitchUrl("http://www.qiniu.com", srcOriginLink)
 
-			srcLogBuffer.WriteString(", resLink is")
-			srcLogBuffer.WriteString(resLink)
-			//logString := srcLogBuffer.String()
-			//log.Println(logString)
-
-			res := reLink.MatchString(resLink)
+			res := ReIsLink(resLink)
 			if res == true {
 
-
-
-				waitUrlSet.Add(resLink)
-
-
+				waitUrlMap[resLink] = 0
 
 			}
 		}
 	}
 
-
 	for i := 0; i < len(hrefArray); i++ {
-		var hrefLogBuffer bytes.Buffer
 
 		hrefOriginLink := hrefArray[i][1]
 
-		hrefLogBuffer.WriteString("hrefOriginLink is ")
-		hrefLogBuffer.WriteString(hrefOriginLink)
-
-		if reSlash.MatchString(hrefOriginLink) != true {
+		if ReHaveSlash(hrefOriginLink) != true {
 			continue
 		}
 
-		if reHttp.MatchString(hrefOriginLink) != true {
+		if ReIsHttp(hrefOriginLink) != true {
 
-			var resUrlBuffer bytes.Buffer
-			resUrlBuffer.WriteString("http://www.qiniu.com")
-			resUrlBuffer.WriteString(hrefOriginLink)
-			resLink := resUrlBuffer.String() // 拼接结果
-			res := reLink.MatchString(resLink)
-
-
-			hrefLogBuffer.WriteString(", resLink is")
-			hrefLogBuffer.WriteString(resLink)
-			//logString := hrefLogBuffer.String()
-			//log.Println(logString)
+			resLink := StitchUrl("http://www.qiniu.com", hrefOriginLink)
+			res := ReIsLink(resLink)
 
 			if res == true {
 
-
-
-				waitUrlSet.Add(resLink)
+				waitUrlMap[resLink] = 0
 			}
 
 		}
 	}
-	fmt.Println("now show urls")
-	
-	fmt.Println(waitUrlSet.Flatten())
-	fmt.Println(waitUrlSet.Len())
 
-	waitUrlArray := waitUrlSet.Flatten()
-	for i :=0; i< len(waitUrlArray);i++ {
-		verifyUrl := waitUrlArray[i]
-		fmt.Println(verifyUrl)
-		test_num += 1
-		fmt.Println(test_num)
-
-
+	for k, v := range waitUrlMap {
+		fmt.Println(k, v)
 	}
-	
-	
-	
+	fmt.Println(len(waitUrlMap))
 
+
+}
+
+type Spider struct {
+	Rooturl string
+}
+
+func StitchUrl(DomainString string, PathString string) (UString string) {
+	var resUrlBuffer bytes.Buffer
+
+	resUrlBuffer.WriteString(DomainString)
+	resUrlBuffer.WriteString(PathString)
+
+	UString = resUrlBuffer.String()
+
+	return UString
+}
+
+func Crawling(UrlString string) (ResponseBodyString string, StatusCode int, ContentType string) {
+	resp, err := http.Get(UrlString)
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return string(body), resp.StatusCode, resp.Header.Get("Content-Type")
+}
+
+func ReHrefSubMatch(s string) [][]string {
+	reHref, _ := regexp.Compile(PATTERN_HERF)
+	hrefArray := reHref.FindAllStringSubmatch(s, 10000)
+
+	return hrefArray
+
+}
+
+func ReSrcSubMatch(s string) [][]string {
+	reHref, _ := regexp.Compile(PATTERN_SRC)
+	srcArray := reHref.FindAllStringSubmatch(s, 10000)
+
+	return srcArray
+
+}
+
+func ReIsHttp(s string) bool {
+	reHttp, _ := regexp.Compile(PATTERN_HTTP)
+	return reHttp.MatchString(s)
+
+}
+
+func ReIsLink(s string) bool  {
+	reLink, _ := regexp.Compile(PATTERN_LINK)
+	return reLink.MatchString(s)
+}
+
+func ReHaveSlash(s string) bool  {
+	reSlash, _ := regexp.Compile(PATTERN_SLASH)
+	return reSlash.MatchString(s)
+
+}
+
+func ExtractBody(s string) ([][]string, [][]string) {
+	hrefArray := ReHrefSubMatch(s)
+	srcArray := ReSrcSubMatch(s)
+
+	return hrefArray, srcArray
+}
+
+
+
+func IterCraw(surl string, wum map[string]int, fum map[string]int){
+	respBody, StatusCode, ContentType := Crawling(surl)
+
+	fum[surl] = StatusCode
+
+	if ContentType == "text/html; charset=utf-8"{
+		//hrefArray, srcArray := ExtractBody(respBody)
+	}else {
+		//hrefArray, srcArray := make([][]string, 0, 0), make([][]string, 0, 0)
+	}
+
+	SaToMap(srcArray, wum)
+
+	HaToMap(hrefArray, wum)
+
+}
+
+func SaToMap(sa [][]string,  wum map[string]int)  {
+	for i := 0; i < len(sa); i++ {
+		srcOriginLink := sa[i][1]
+
+		if ReHaveSlash(srcOriginLink) != true {
+			continue
+		}
+
+		if ReIsHttp(srcOriginLink) != true {
+
+			resLink := StitchUrl("http://www.qiniu.com", srcOriginLink)
+
+			res := ReIsLink(resLink)
+			if res == true {
+
+				wum[resLink] = 0
+
+			}
+		}
+	}
+
+}
+
+func HaToMap(ha [][]string,  wum map[string]int)  {
+	for i := 0; i < len(ha); i++ {
+
+		hrefOriginLink := ha[i][1]
+
+		if ReHaveSlash(hrefOriginLink) != true {
+			continue
+		}
+
+		if ReIsHttp(hrefOriginLink) != true {
+
+			resLink := StitchUrl("http://www.qiniu.com", hrefOriginLink)
+			res := ReIsLink(resLink)
+
+			if res == true {
+
+				wum[resLink] = 0
+			}
+		}
+	}
 
 }
