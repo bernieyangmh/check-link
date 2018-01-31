a6a20293babec922bcb74215ba72fb59253ab41a
bernieyangmh <berniey@163.com>
2018-01-31 10:33:25 +0800

    add dir

--- src/check-link/commons.go
+++ src/check-link/commons.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 	"unicode"
+	"fmt"
 )
 
 //输入一个链接，将状态码放进map，能爬取的链接输进管道
@@ -180,6 +181,77 @@ func DomArrayToUrl(cU CUrl, a []CUrl, cH chan<- CUrl, tM map[string]int) {
 			}
 		} else {
 			log.Print("ErrorPath			" + ha)
+		}
+	}
+}
+
+func DailyCheck() {
+	type Item struct {
+		CrawlUrl string `bson:"crawl_url"`
+		RefUrl   string `json:"RefUrl" bson:"ref_url"`
+	}
+	item := Item{}
+	items := GetIterUrl()
+	for items.Next(&item) {
+		url := item.CrawlUrl
+		ResponseBodyString, StatusCode, _ := Crawling(url)
+
+		fmt.Println("\n")
+		fmt.Println(url)
+		fmt.Println(item.RefUrl)
+		fmt.Println(StatusCode)
+		if StatusCode == -2 {
+			fmt.Println(ResponseBodyString)
+		}
+		fmt.Println("\n\n----------------------------------------------")
+
+	}
+}
+
+
+
+func LanuchCrawl() {
+
+	var ROOT_DOMAIN = [2]string{"https://www.qiniu.com", "https://developer.qiniu.com"}
+
+	var executeChannel = make(chan CUrl, 5000)
+	var trailMap = make(map[string]int)
+	var finishArray = make([]CUrl, 0, 10000)
+	var errorArryay = make([]CUrl, 0, 1000)
+
+	firCrawl := CUrl{CrawlUrl: ROOT_DOMAIN[0]}
+	secCrawl := CUrl{CrawlUrl: ROOT_DOMAIN[1]}
+	//将根域名放入channel
+	PutChannel(firCrawl, executeChannel)
+	PutChannel(secCrawl, executeChannel)
+
+	for len(executeChannel) > 0 {
+		aimUrl := GetChannel(executeChannel)
+		if aimUrl.CrawlUrl != "close" {
+			IterCrawl(aimUrl, trailMap, executeChannel, &finishArray, &errorArryay)
+			fmt.Println(len(executeChannel))
+		}
+	}
+
+	for i := 0; i < len(finishArray); i++ {
+		fmt.Println(finishArray[i])
+		err := finishArray[i].Insert()
+		if err != nil {
+			log.Println(err)
+
+		}
+	}
+
+	log.Println("/n url num is %d/n", len(finishArray))
+
+	for i := 0; i < len(errorArryay); i++ {
+		if errorArryay[i].StatusCode != 0 {
+			fmt.Println(errorArryay[i].CrawlUrl)
+			fmt.Println(errorArryay[i].RefUrl)
+			fmt.Println(errorArryay[i].StatusCode)
+			fmt.Println(errorArryay[i].Context)
+			fmt.Println(errorArryay[i].QueryError)
+			fmt.Println("\n")
 		}
 	}
 }
--- src/check-link/nets.go
+++ src/check-link/nets.go
@@ -39,10 +39,6 @@ func IterCrawl(cu CUrl, tM map[string]int, cH chan<- CUrl, fA *[]CUrl, eA *[]CUr
 		*eA = append(*eA, cu)
 	}
 
-	//err = cu.Insert()
-	//if err != nil{
-	//	log.Println("Insert		" + err.Error())
-	//}
 
 	//如果链接主域名在爬取列表内，Content-Type为html且不在trailMap内，进入读取
 	if (ContentType == "text/html; charset=utf-8") && (tM[cu.CrawlUrl] != 0) && ReDomainMatch(cu.CrawlUrl) {
@@ -131,71 +127,6 @@ func GetFromRedirectUrl(lu string, rn int) (string, int, string) {
 	return "xxxnohtml", resp.StatusCode, resp.Header.Get("Content-Type")
 }
 
-func LanuchCrawl() {
 
-	var ROOT_DOMAIN = [2]string{"https://www.qiniu.com", "https://developer.qiniu.com"}
 
-	var executeChannel = make(chan CUrl, 5000)
-	var trailMap = make(map[string]int)
-	var finishArray = make([]CUrl, 0, 10000)
-	var errorArryay = make([]CUrl, 0, 1000)
 
-	firCrawl := CUrl{CrawlUrl: ROOT_DOMAIN[0]}
-	secCrawl := CUrl{CrawlUrl: ROOT_DOMAIN[1]}
-	//将根域名放入channel
-	PutChannel(firCrawl, executeChannel)
-	PutChannel(secCrawl, executeChannel)
-
-	for len(executeChannel) > 0 {
-		aimUrl := GetChannel(executeChannel)
-		if aimUrl.CrawlUrl != "close" {
-			IterCrawl(aimUrl, trailMap, executeChannel, &finishArray, &errorArryay)
-			fmt.Println(len(executeChannel))
-		}
-	}
-
-	for i := 0; i < len(finishArray); i++ {
-		fmt.Println(finishArray[i])
-		err := finishArray[i].Insert()
-		if err != nil {
-			log.Println(err)
-
-		}
-	}
-
-	log.Println("/n url num is %d/n", len(finishArray))
-
-	for i := 0; i < len(errorArryay); i++ {
-		if errorArryay[i].StatusCode != 0 {
-			log.Println(errorArryay[i].CrawlUrl)
-			fmt.Println(errorArryay[i].RefUrl)
-			fmt.Println(errorArryay[i].StatusCode)
-			fmt.Println(errorArryay[i].Context)
-			fmt.Println(errorArryay[i].QueryError)
-			fmt.Println("\n")
-		}
-	}
-}
-
-func DailyCheck() {
-	type Item struct {
-		CrawlUrl string `bson:"crawl_url"`
-		RefUrl   string `json:"RefUrl" bson:"ref_url"`
-	}
-	item := Item{}
-	items := GetIterUrl()
-	for items.Next(&item) {
-		url := item.CrawlUrl
-		ResponseBodyString, StatusCode, _ := Crawling(url)
-
-		fmt.Println("\n")
-		fmt.Println(url)
-		fmt.Println(item.RefUrl)
-		fmt.Println(StatusCode)
-		if StatusCode == -2 {
-			fmt.Println(ResponseBodyString)
-		}
-		fmt.Println("\n\n----------------------------------------------")
-
-	}
-}
